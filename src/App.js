1de7f694fbbf4bb940726bed8ffb7fd9a3ae7f41
Conor Le <lethanhvu2004@gmail.com>
2024-10-09 19:58:09 -0400

    added COMP3104

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101411302 || Conor Le || DevOps</h5>
+      <h5>Developed By: 101411302 || Conor Le || DevOps-COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
